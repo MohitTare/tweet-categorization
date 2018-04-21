5329e4df2d083bcf780e1430c40c8c4df0235399
Mohit-HP\Mohit <mohittare92@gmail.com>
2018-04-21 14:16:28 +0530

    changed repo structure

--- MLPipelineDemo/MLValidate/MLValidate.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import pandas as pd
-import numpy as np
-from sklearn.metrics import classification_report
-
-def modelValidate(y_pred,y_actual,metric_type):
-    if str(metric_type) == 'classification':
-        return classification_report(y_actual, y_pred)
